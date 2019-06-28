d91f79409b8dd924756ac321bddb7b9707fc4ed8
Kun Lee <45495755+dankunlee@users.noreply.github.com>
2019-06-28 16:55:50 -0400

    Update path.h

--- path.h
+++ path.h
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+enum CPUStates{
+	S_USER = 1,
+	S_NICE,
+	S_SYSTEM,
+	S_IDLE,
+	S_IOWAIT,
+	S_IRQ,
+	S_SOFTIRQ,
+	S_STEAL,
+	S_GUEST,
+	S_GUEST_NICE
+};
+
 //Class for providing various paths
 class Path {
     public: 
